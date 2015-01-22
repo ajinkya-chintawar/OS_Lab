ae7d70165f70af62093f914730620d9dd14ac13b
Ajinkya Chintawar <ajinkya-chintawar>
2015-01-23 01:48:23 +0530

    GD,LR,SR done

--- os_phase1.cpp
+++ os_phase1.cpp
@@ -2,15 +2,19 @@
 #include<vector>
 #include<cstring>
 #include<fstream>
+#include<stdlib.h>
 
 using namespace std;
 void load();
 int find();
 void startexecution();
 void executeuserprogram();
+void MOS(int );
+void calculate_address();
 char memory[100][4],ir[4];
-string next_to_amj="cdss";
-int blocks=0,ic=0,c=0,m=0;
+string next_to_amj="cdss",data;
+int blocks=0,ic=0,c=0,m=0,R,data_counter,address,row_no,col_no;
+ifstream infile("test.txt");
 int main()
 {
 	load();
@@ -37,8 +41,7 @@ void load()
 {
 	int flag=0;
 
-	ifstream infile("test.txt");
-
+	
 	while (infile)
   	{
 
@@ -69,7 +72,7 @@ void load()
 	    		}    		
 	    		if(!find()) //After reading particular line if
 	    			break;	//next_to_amj is H then instructions are finished
-	    			
+				    		//memory[m][0] = 'H'; 		
     		}
     	}		
     	else		// Start executing control cards    	
@@ -86,10 +89,83 @@ void startexecution()
 void executeuserprogram()
 {
 	for(int ic = 0;ic < m; ic++)
+	{
+		string temp,temp1,instruction;
 		for(int j=0;j<4;j++)
+			ir[j] = memory[ic][j];			
+		temp = ir[0];
+		temp1 = ir[1];
+		instruction = temp + temp1;
+		cout<<instruction<<endl;
+		if(instruction == "GD")
 		{
-			ir[j] = memory[ic][j];
-			
+			MOS(1);
 		}
-	
+		else if(instruction == "PD")
+		{
+			MOS(2);
+		}
+		else if(instruction == "LR")
+		{
+			calculate_address();
+			R = (int)memory[row_no][col_no];
+			cout<<"value of r is "<<R<<endl;
+		}
+		else if(instruction == "SR")
+		{
+			calculate_address();			
+			memory[row_no][col_no] = (char)R;			
+			cout<<"value of mem is "<<memory[row_no][col_no]<<endl;
+		}
+		 
+	}
 }
+void MOS(int si)
+{
+	switch(si)
+	{
+		case 1:		//GD Operation will take place here 
+				{		//i.e reading a line after DTA pointed by data_counter
+					string temp;
+					calculate_address();			
+					getline(infile,data);
+					cout<<"Data is "<<data<<endl;
+					temp = data.substr(0,4);		
+					if(temp == "$END" || temp == "")
+					{
+						cout<<"Insufficient Arguments Passed";
+						exit(0);
+					}					
+					if(row_no > 100)
+					{
+						cout<<"Memory is exceeded"<<endl;
+						cout<<"row no "<<row_no<<endl;
+						exit(0);
+					}
+					int counter = 0;
+					for(int i = row_no;counter<(data.length()-1);i++)
+						for(int j = 0;j<4 && counter<(data.length()-1);j++)
+						{
+							memory[i][j] = data[counter];
+							counter += 1;
+							cout<<"i is\t"<<i<<"j is "<<j<<"\t"<<data.length()<<endl;
+							
+						}
+					break;
+				}
+
+		case 2:
+				cout<<"Read Data Here";
+		case 3:
+				cout<<"Halt Here";
+					
+	}
+	infile.close();
+}
+void calculate_address()
+{
+	address = (ir[2]-48) * 10 + (ir[3] - 48);
+	row_no = address / 4;
+	col_no = address % 4;	
+}
+
