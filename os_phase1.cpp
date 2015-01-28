6538a6ba77af59940b707d69984834ab831095fb
Ajinkya Chintawar <ajinkya-chintawar>
2015-01-29 04:50:38 +0530

    Done for multiple programs

--- os_phase1.cpp
+++ os_phase1.cpp
@@ -11,7 +11,7 @@ void startexecution();
 void executeuserprogram();
 void MOS(int );
 void calculate_address();
-char memory[100][4],ir[4],c,R;
+char memory[100][4]={0},ir[4],c,R;
 string next_to_amj="cdss",data;
 int blocks=0,ic=0,m=0,data_counter,address,row_no,col_no;
 ifstream infile("test.txt");
@@ -23,11 +23,11 @@ int main()
 	infile.close();
 	outfile.close();
 }
-int find()
+int find(string trimmed_string)
 {
 	string :: iterator it;
-	cout<<"String is " <<next_to_amj<<endl;
-	for (it= next_to_amj.begin(); it != next_to_amj.end(); it++)
+	cout<<"String is " <<trimmed_string<<endl;
+	for (it= trimmed_string.begin(); it != trimmed_string.end(); it++)
 		if(*it == 'H')
 		{
 			cout<<"returning 0"<<endl;
@@ -52,30 +52,41 @@ void load()
 										 // the actions to be perform    	
     	if(temp == "$END")	//break the while loop
 		{
-			break;
+			//Reset all values here
+			memory[100][4]={0};
+			ir[4]={0};
+			ic=m=data_counter=address=0;
+			outfile<<"\n\n";
+			continue;
 		}
     	else if(temp == "$AMJ") //for $AMJ go on accepting
     							//from file till you get H
     							//And store it in memory initial blocks
     	{
+    		flag=0;
     		while(getline( infile, next_to_amj))  //Read File till we get H  		
     		{
-    			while(next_to_amj != "" && find()) //Read lines next to amj
+    			while(next_to_amj != "") //Read lines next to amj
     			{
 	    			trimmed_string = next_to_amj.substr(0,4);
+	    			if(!find(trimmed_string))
+	    			{
+	    				flag=1;
+	    				break;
+	    			}
 	    			next_to_amj.erase(0,4);
 					memory[m][0]=trimmed_string[0]; //Store this instructions
 					memory[m][1]=trimmed_string[1];	//in memory which is available   			
 	    			memory[m][2]=trimmed_string[2];
 	    			memory[m][3]=trimmed_string[3];
 	    			m++;
-	    			if(m % 10 == 0)
-	    				blocks++; 	//1 block = 10 m
+	    			//if(m % 10 == 0)
+	    			//	blocks++; 	//1 block = 10 m
 	    		}    		
-	    		if(!find()) //After reading particular line if
-	    			break;	//next_to_amj is H then instructions are finished
-				    		//memory[m][0] = 'H'; 		
+	    		if(flag == 1)	//We got h as substring in inner loop so break
+					break; 		
     		}
+    	
     	}		
     	else		// Start executing control cards    	
     		startexecution();
@@ -98,7 +109,7 @@ void executeuserprogram()
 		temp = ir[0];
 		temp1 = ir[1];
 		instruction = temp + temp1;
-		cout<<instruction<<endl;
+		cout<<instruction<<ir[2]<<ir[3]<<endl;
 		if(instruction == "GD")
 		{
 			MOS(1);
@@ -116,7 +127,8 @@ void executeuserprogram()
 		else if(instruction == "SR")
 		{
 			calculate_address();			
-			memory[row_no][col_no] = R;			
+			memory[row_no][col_no] = R;
+			cout<<"In sr row nd cols are "<<row_no<<"\t"<<col_no<<endl;
 			cout<<"value of mem is "<<memory[row_no][col_no]<<endl;
 		}
 		else if(instruction == "CR")
@@ -142,13 +154,13 @@ void MOS(int si)
 	{
 		case 1:		//GD Operation will take place here 
 				{		//i.e reading a line after DTA pointed by data_counter
-					string temp,data;
+					string temp;
+					data = "";
 					ir[3] = 0;					
 					calculate_address();			
 					getline(infile,data);
 					cout<<"Data is "<<data<<endl;
 					temp = data.substr(0,4);
-					cout<<"temppppp "<<temp<<endl;
 					if(temp == "$END" || temp == "")
 					{
 						cout<<"Insufficient Arguments Passed";
@@ -176,7 +188,8 @@ void MOS(int si)
 		case 2:			// In PD Data is written to output file
 						// from ex 30 to 39 mem location (10 words)
 				{
-					cout<<"In PD"<<endl;
+					string space = " ";
+					cout<<"In PD"<<ir[0]<<ir[1]<<ir[2]<<ir[3]<<endl;
 					ir[3] = 0;
 					calculate_address();
 					int count = 0;
@@ -186,9 +199,20 @@ void MOS(int si)
 						count++;
 						for(int col = 0;col < 4;col++)
 						{
-							outfile<<memory[row][col];
+							if(memory[row][col] == ' ')
+							{
+								//cout<<"\t";
+								outfile<<"\t";
+							}
+							else
+							{
+								cout<<"Writting "<<memory[row][col]<<endl;
+								outfile<<memory[row][col];
+							}	
 						}
 					}
+					outfile<<"\n";
+					break;
 				}
 				
 		case 3:
